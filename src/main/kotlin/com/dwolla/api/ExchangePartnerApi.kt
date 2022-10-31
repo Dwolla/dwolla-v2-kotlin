f5bcbd3247d8bd084268c040821d3f188707943d
James Coon <james@jcoon.dev>
2022-10-31 16:14:33 -0400

    Update spacing for ExchangePartnersApi

--- src/main/kotlin/com/dwolla/api/ExchangePartnerApi.kt
+++ src/main/kotlin/com/dwolla/api/ExchangePartnerApi.kt
@@ -7,17 +7,11 @@ import com.dwolla.util.Paths.Companion.EXCHANGE_PARTNERS
 
 class ExchangePartnerApi(private val client: DwollaClient) {
     fun get(id: String): ExchangePartner {
-        return client.get(
-            ExchangePartner::class.java,
-            exchangePartnerUrl(id)
-        ).body
+        return client.get(ExchangePartner::class.java, exchangePartnerUrl(id)).body
     }
 
     fun list(): ExchangePartners {
-        return client.get(
-            ExchangePartners::class.java,
-            EXCHANGE_PARTNERS
-        ).body
+        return client.get(ExchangePartners::class.java, EXCHANGE_PARTNERS).body
     }
 
     private fun exchangePartnerUrl(id: String): String {
