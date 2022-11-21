23c9f2c37a3e003c68e13aa21e2d9ad14dd66048
James Coon <james@jcoon.dev>
2022-11-21 16:38:43 -0500

    Update Exchange Partners API class file name and include @Throws in method signatures

--- src/main/kotlin/com/dwolla/Dwolla.kt
+++ src/main/kotlin/com/dwolla/Dwolla.kt
@@ -22,7 +22,7 @@ class Dwolla(
     @JvmField val customers = CustomersApi(this)
     @JvmField val documents = DocumentsApi(this)
     @JvmField val exchanges = ExchangesApi(this)
-    @JvmField val exchangePartners = ExchangePartnerApi(this)
+    @JvmField val exchangePartners = ExchangePartnersApi(this)
     @JvmField val fundingSources = FundingSourcesApi(this)
     @JvmField val fundingSourcesTokens = FundingSourcesTokensApi(this)
     @JvmField val iavTokens = IavTokensApi(this)
