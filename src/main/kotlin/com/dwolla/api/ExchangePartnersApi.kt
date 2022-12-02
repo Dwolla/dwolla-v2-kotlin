33fc8a3875e691465bdc3f5e450ef382ab0bbc45
James Coon <james@jcoon.dev>
2022-12-02 14:56:43 -0500

    DEV-1172: Update Exchange Partners API class file name and include `@Throws` in method signatures (#58)

--- src/main/kotlin/com/dwolla/api/ExchangePartnersApi.kt
+++ src/main/kotlin/com/dwolla/api/ExchangePartnersApi.kt
@@ -1,19 +1,24 @@
 package com.dwolla.api
 
 import com.dwolla.DwollaClient
+import com.dwolla.exception.DwollaApiException
+import com.dwolla.exception.DwollaAuthException
 import com.dwolla.resource.exchanges.ExchangePartner
 import com.dwolla.resource.exchanges.ExchangePartners
 import com.dwolla.util.Paths.Companion.EXCHANGE_PARTNERS
 
-class ExchangePartnerApi(private val client: DwollaClient) {
+class ExchangePartnersApi(private val client: DwollaClient) {
+    @Throws(DwollaApiException::class, DwollaAuthException::class)
     fun get(id: String): ExchangePartner {
         return client.get(ExchangePartner::class.java, exchangePartnerUrl(id)).body
     }
 
+    @Throws(DwollaApiException::class, DwollaAuthException::class)
     fun list(): ExchangePartners {
         return client.get(ExchangePartners::class.java, EXCHANGE_PARTNERS).body
     }
 
+    @Throws(DwollaApiException::class, DwollaAuthException::class)
     private fun exchangePartnerUrl(id: String): String {
         return client.urlBuilder.buildUrl(EXCHANGE_PARTNERS, id)
     }
