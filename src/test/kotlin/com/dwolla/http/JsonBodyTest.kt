8d422f2baa20c64efc33ce40badc1c89dec14aa8
James Coon <james@jcoon.dev>
2023-01-27 16:11:06 -0500

    Move test to new package and add tag

--- src/test/kotlin/com/dwolla/http/JsonBodyTest.kt
+++ src/test/kotlin/com/dwolla/http/JsonBodyTest.kt
@@ -1,11 +1,13 @@
-package com.dwolla.util
+package com.dwolla.http
 
-import com.dwolla.http.JsonBody
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Tags
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@Tags(Tag("http"))
 class JsonBodyTest {
     @Test
     fun `builds from Gson JsonObject`() {
